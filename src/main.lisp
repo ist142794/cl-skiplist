d5697767c4b29b45f827360b9c71a734f1d4615f
rcco <rodrigo.coutinho.correia@gmail.com>
2016-04-26 00:54:36 +0100

    Documented main file

--- src/main.lisp
+++ src/main.lisp
@@ -1,2 +1,19 @@
+;; -*- mode: lisp-mode; coding: utf-8 -*-
+
+;;------------------------------------------------------------------------------
+;;
+;; NAME
+;;	OLA
+;;
+;; ARGS
+;;	argumento - recebe qualquer coisa
+;;
+;; RETURN
+;;	void
+;;
+;; DESCRIPTION
+;;	Awsome function that print its argumento to the standard output.
+;;
+;;------------------------------------------------------------------------------
 (defun ola (argumento)
-  (format t "Olá ~a~%" argumento))
+  (format t "Hello ~a~%" argumento))
